69470683218f8d8cbba840ca81a1799f7a2f1428
Luann Gonçalves <luann2v2v@hotmail.com>
2025-05-06 20:49:54 -0300

    Commit inicial do projeto ASP.NET MVC

---
+++ src/AspMvcProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AspMvcProject.Models;
+
+namespace AspMvcProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Aluno { get; set; }
+    }
+}
